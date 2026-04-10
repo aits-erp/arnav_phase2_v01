7bb9a63fa09553bb010e0b9314cb3fe29937e003
Sukeshanee <zende.sukeshanee@kgce.edu.in>
2026-04-10 19:15:52 +0000

    Added Repair Receipt Doctype with fields and image support

---
+++ daily_reconciliation/daily_reconciliation/doctype/repair_and_service/repair_and_service.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Sukku and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Repair and Service", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ daily_reconciliation/daily_reconciliation/doctype/repair_and_service/test_repair_and_service.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Sukku and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRepairandService(FrappeTestCase):
+	pass
