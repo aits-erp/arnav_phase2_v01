1293737a765702a7997373d69d26aada45f27472
Sukeshanee <zende.sukeshanee@kgce.edu.in>
2026-04-22 06:02:41 +0000

    Updated daily_reconciliation app code

--- daily_reconciliation/daily_reconciliation/doctype/repair_and_service/repair_and_service.js
+++ daily_reconciliation/daily_reconciliation/doctype/repair_and_service/repair_and_service.js
@@ -1,8 +1,357 @@
-// Copyright (c) 2026, Sukku and contributors
-// For license information, please see license.txt
 
 // frappe.ui.form.on("Repair and Service", {
-// 	refresh(frm) {
 
-// 	},
+//     refresh(frm) {
+
+//         // Show POS No only when Entry Type = POS
+//         frm.toggle_display("pos_no", frm.doc.entry_type === "POS");
+
+//         // Only after Save
+//         if (!frm.doc.__islocal) {
+
+//             // =====================================================
+//             // BUTTON 1 : RECEIVE PRODUCT
+//             // =====================================================
+//             if (!frm.doc.inward_stock_entry) {
+
+//                 frm.add_custom_button("Receive Product", function () {
+
+//                     frappe.confirm(
+//                         "Create Stock In Entry for this Repair Job?",
+//                         function () {
+
+//                             frappe.call({
+//                                 method: "daily_reconciliation.daily_reconciliation.doctype.repair_and_service.repair_and_service.make_stock_in",
+//                                 args: {
+//                                     docname: frm.doc.name
+//                                 },
+//                                 freeze: true,
+//                                 freeze_message: "Creating Stock In Entry...",
+
+//                                 callback(r) {
+
+//                                     if (r.message) {
+
+//                                         frappe.show_alert({
+//                                             message: "Stock In Created : " + r.message,
+//                                             indicator: "green"
+//                                         });
+
+//                                         frm.reload_doc();
+
+//                                         frappe.set_route(
+//                                             "Form",
+//                                             "Stock Entry",
+//                                             r.message
+//                                         );
+//                                     }
+//                                 }
+//                             });
+
+//                         },
+//                         function () {
+//                             frappe.show_alert({
+//                                 message: "Cancelled",
+//                                 indicator: "orange"
+//                             });
+//                         }
+//                     );
+
+//                 }).addClass("btn-primary");
+//             }
+
+//             // =====================================================
+//             // BUTTON 2 : RETURN TO CUSTOMER
+//             // =====================================================
+//             if (frm.doc.inward_stock_entry && !frm.doc.outward_stock_entry) {
+
+//                 frm.add_custom_button("Return To Customer", function () {
+
+//                     frappe.confirm(
+//                         "Create Stock Out Entry and Deliver Product?",
+//                         function () {
+
+//                             frappe.call({
+//                                 method: "daily_reconciliation.daily_reconciliation.doctype.repair_and_service.repair_and_service.make_stock_out",
+//                                 args: {
+//                                     docname: frm.doc.name
+//                                 },
+//                                 freeze: true,
+//                                 freeze_message: "Creating Stock Out Entry...",
+
+//                                 callback(r) {
+
+//                                     if (r.message) {
+
+//                                         frappe.show_alert({
+//                                             message: "Stock Out Created : " + r.message,
+//                                             indicator: "green"
+//                                         });
+
+//                                         frm.reload_doc();
+
+//                                         frappe.set_route(
+//                                             "Form",
+//                                             "Stock Entry",
+//                                             r.message
+//                                         );
+//                                     }
+//                                 }
+//                             });
+
+//                         },
+//                         function () {
+//                             frappe.show_alert({
+//                                 message: "Cancelled",
+//                                 indicator: "orange"
+//                             });
+//                         }
+//                     );
+
+//                 }).addClass("btn-success");
+//             }
+//         }
+//     },
+
+//     // =====================================================
+//     // ENTRY TYPE CHANGE
+//     // =====================================================
+//     entry_type(frm) {
+//         frm.toggle_display("pos_no", frm.doc.entry_type === "POS");
+//     },
+
+//     // =====================================================
+//     // FETCH POS DETAILS
+//     // =====================================================
+//     pos_no(frm) {
+
+//         if (!frm.doc.pos_no) return;
+
+//         frappe.call({
+//             method: "daily_reconciliation.daily_reconciliation.doctype.repair_and_service.repair_and_service.get_pos_details",
+//             args: {
+//                 pos_no: frm.doc.pos_no
+//             },
+
+//             freeze: true,
+//             freeze_message: "Fetching POS Details...",
+
+//             callback(r) {
+
+//                 if (r.message) {
+
+//                     let d = r.message;
+
+//                     frm.set_value("name1", d.client_name || "");
+//                     frm.set_value("phone", d.mobile_number || "");
+//                     frm.set_value("address", d.address || "");
+//                     frm.set_value("branch", d.branch || "");
+
+//                     frm.set_value("product_name", d.product_name || "");
+//                     frm.set_value("sku_code", d.sku_code || "");
+//                     frm.set_value("gross_weight", d.gross_weight || 0);
+//                     frm.set_value("net_weight", d.net_weight || 0);
+
+//                     frappe.show_alert({
+//                         message: "POS Details Loaded",
+//                         indicator: "green"
+//                     });
+//                 }
+//             }
+//         });
+//     }
+
 // });
+
+
+
+frappe.ui.form.on("Repair and Service", {
+
+    // =====================================================
+    // ON LOAD - SET CURRENT DATE IN DATE FIELD
+    // =====================================================
+    onload(frm) {
+        if (frm.is_new() && !frm.doc.date) {
+            frm.set_value("date", frappe.datetime.get_today());
+        }
+    },
+
+    // =====================================================
+    // REFRESH
+    // =====================================================
+    refresh(frm) {
+
+        // Set current date if blank
+        if (frm.is_new() && !frm.doc.date) {
+            frm.set_value("date", frappe.datetime.get_today());
+        }
+
+        // Show POS No only when Entry Type = POS
+        frm.toggle_display("pos_no", frm.doc.entry_type === "POS");
+
+        // Only after Save
+        if (!frm.doc.__islocal) {
+
+            // =====================================================
+            // BUTTON 1 : RECEIVE PRODUCT
+            // =====================================================
+            if (!frm.doc.inward_stock_entry) {
+
+                frm.add_custom_button("Receive Product", function () {
+
+                    frappe.confirm(
+                        "Create Stock In Entry for this Repair Job?",
+                        function () {
+
+                            frappe.call({
+                                method: "daily_reconciliation.daily_reconciliation.doctype.repair_and_service.repair_and_service.make_stock_in",
+                                args: {
+                                    docname: frm.doc.name
+                                },
+                                freeze: true,
+                                freeze_message: "Creating Stock In Entry...",
+
+                                callback(r) {
+
+                                    if (r.message) {
+
+                                        frappe.show_alert({
+                                            message: "Stock In Created : " + r.message,
+                                            indicator: "green"
+                                        });
+
+                                        frm.reload_doc();
+
+                                        frappe.set_route(
+                                            "Form",
+                                            "Stock Entry",
+                                            r.message
+                                        );
+                                    }
+                                }
+                            });
+
+                        },
+                        function () {
+                            frappe.show_alert({
+                                message: "Cancelled",
+                                indicator: "orange"
+                            });
+                        }
+                    );
+
+                }).addClass("btn-primary");
+            }
+
+            // =====================================================
+            // BUTTON 2 : RETURN TO CUSTOMER
+            // =====================================================
+            if (frm.doc.inward_stock_entry && !frm.doc.outward_stock_entry) {
+
+                frm.add_custom_button("Return To Customer", function () {
+
+                    frappe.confirm(
+                        "Create Stock Out Entry and Deliver Product?",
+                        function () {
+
+                            frappe.call({
+                                method: "daily_reconciliation.daily_reconciliation.doctype.repair_and_service.repair_and_service.make_stock_out",
+                                args: {
+                                    docname: frm.doc.name
+                                },
+                                freeze: true,
+                                freeze_message: "Creating Stock Out Entry...",
+
+                                callback(r) {
+
+                                    if (r.message) {
+
+                                        frappe.show_alert({
+                                            message: "Stock Out Created : " + r.message,
+                                            indicator: "green"
+                                        });
+
+                                        frm.reload_doc();
+
+                                        frappe.set_route(
+                                            "Form",
+                                            "Stock Entry",
+                                            r.message
+                                        );
+                                    }
+                                }
+                            });
+
+                        },
+                        function () {
+                            frappe.show_alert({
+                                message: "Cancelled",
+                                indicator: "orange"
+                            });
+                        }
+                    );
+
+                }).addClass("btn-success");
+            }
+        }
+    },
+
+    // =====================================================
+    // BEFORE SAVE - FORCE CURRENT DATE
+    // =====================================================
+    before_save(frm) {
+        frm.set_value("date", frappe.datetime.get_today());
+    },
+
+    // =====================================================
+    // ENTRY TYPE CHANGE
+    // =====================================================
+    entry_type(frm) {
+        frm.toggle_display("pos_no", frm.doc.entry_type === "POS");
+    },
+
+    // =====================================================
+    // FETCH POS DETAILS
+    // =====================================================
+    pos_no(frm) {
+
+        if (!frm.doc.pos_no) return;
+
+        frappe.call({
+            method: "daily_reconciliation.daily_reconciliation.doctype.repair_and_service.repair_and_service.get_pos_details",
+            args: {
+                pos_no: frm.doc.pos_no
+            },
+
+            freeze: true,
+            freeze_message: "Fetching POS Details...",
+
+            callback(r) {
+
+                if (r.message) {
+
+                    let d = r.message;
+
+                    frm.set_value("name1", d.client_name || "");
+                    frm.set_value("phone", d.mobile_number || "");
+                    frm.set_value("address", d.address || "");
+                    frm.set_value("branch", d.branch || "");
+
+                    frm.set_value("product_name", d.product_name || "");
+                    frm.set_value("sku_code", d.sku_code || "");
+                    frm.set_value("gross_weight", d.gross_weight || 0);
+                    frm.set_value("net_weight", d.net_weight || 0);
+
+                    frappe.show_alert({
+                        message: "POS Details Loaded",
+                        indicator: "green"
+                    });
+                }
+            }
+        });
+    }
+
+});
+
+
+
--- daily_reconciliation/daily_reconciliation/doctype/repair_and_service/repair_and_service.py
+++ daily_reconciliation/daily_reconciliation/doctype/repair_and_service/repair_and_service.py
@@ -1,9 +1,142 @@
-# Copyright (c) 2026, Sukku and contributors
-# For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class RepairandService(Document):
-	pass
+    pass
+
+
+# ==========================================================
+# GET POS DETAILS
+# ==========================================================
+@frappe.whitelist()
+def get_pos_details(pos_no):
+
+    pos = frappe.get_doc("POS", pos_no)
+
+    item = {}
+
+    if pos.sku_details and len(pos.sku_details) > 0:
+        row = pos.sku_details[0]
+
+        item = {
+            "product_name": row.product or "",
+            "sku_code": row.sku or "",
+            "gross_weight": row.gross_weight or 0,
+            "net_weight": row.net_weight or 0
+        }
+
+    return {
+        "client_name": pos.client_name or "",
+        "mobile_number": pos.mobile_number or "",
+        "address": pos.address or "",
+        "branch": pos.branch or "",
+        **item
+    }
+
+
+# ==========================================================
+# COMMON FUNCTION
+# SKU → GET ITEM + BATCH + WAREHOUSE
+# ==========================================================
+def get_sku_data(sku_code):
+
+    if not sku_code:
+        frappe.throw("SKU Code is required")
+
+    sku = frappe.get_doc("SKU", sku_code)
+
+    item_code = sku.product
+    batch_no = sku.batch_no
+    warehouse = sku.warehouse
+
+    if not item_code:
+        frappe.throw(f"Item not linked in SKU : {sku_code}")
+
+    return {
+        "item_code": item_code,
+        "batch_no": batch_no,
+        "warehouse": warehouse
+    }
+
+
+# ==========================================================
+# RECEIVE PRODUCT
+# ==========================================================
+@frappe.whitelist()
+def make_stock_in(docname):
+
+    doc = frappe.get_doc("Repair and Service", docname)
+
+    if doc.inward_stock_entry:
+        return doc.inward_stock_entry
+
+    data = get_sku_data(doc.sku_code)
+
+    target_warehouse = doc.branch or data["warehouse"]
+
+    if not target_warehouse:
+        frappe.throw("Warehouse missing in Repair Form / SKU")
+
+    se = frappe.new_doc("Stock Entry")
+    se.stock_entry_type = "Material Receipt"
+    se.company = frappe.defaults.get_user_default("Company")
+
+    se.append("items", {
+        "item_code": data["item_code"],
+        "qty": 1,
+        "batch_no": data["batch_no"],
+        "t_warehouse": target_warehouse,
+        "allow_zero_valuation_rate": 1
+    })
+
+    se.insert(ignore_permissions=True)
+    se.submit()
+
+    doc.db_set("inward_stock_entry", se.name)
+    doc.db_set("repair_status", "In Progress")
+
+    return se.name
+
+
+# ==========================================================
+# RETURN TO CUSTOMER
+# ==========================================================
+@frappe.whitelist()
+def make_stock_out(docname):
+
+    doc = frappe.get_doc("Repair and Service", docname)
+
+    if doc.outward_stock_entry:
+        return doc.outward_stock_entry
+
+    if not doc.inward_stock_entry:
+        frappe.throw("Please Receive Product First")
+
+    data = get_sku_data(doc.sku_code)
+
+    source_warehouse = doc.branch or data["warehouse"]
+
+    if not source_warehouse:
+        frappe.throw("Warehouse missing in Repair Form / SKU")
+
+    se = frappe.new_doc("Stock Entry")
+    se.stock_entry_type = "Material Issue"
+    se.company = frappe.defaults.get_user_default("Company")
+
+    se.append("items", {
+        "item_code": data["item_code"],
+        "qty": 1,
+        "batch_no": data["batch_no"],
+        "s_warehouse": source_warehouse,
+        "allow_zero_valuation_rate": 1
+    })
+
+    se.insert(ignore_permissions=True)
+    se.submit()
+
+    doc.db_set("outward_stock_entry", se.name)
+    doc.db_set("repair_status", "Delivered")
+
+    return se.name
